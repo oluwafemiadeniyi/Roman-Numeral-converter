d8b91f00e8e5ce21491b0a1d920ba24c149c89bf
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 13:59:59 +0100

    create roman numeral objects and keys list

--- roman numeral converter.js
+++ roman numeral converter.js
@@ -1,2 +1,6 @@
 //create  function convertToRoman
-function convertToRoman(num) {}
+function convertToRoman(num) {
+    //create roman numeral objects and keys list
+    let romobj={1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+ let key=Object.keys(romobj).sort((a,b)=>b-a) 
+}
